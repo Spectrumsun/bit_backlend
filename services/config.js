5efae080e0d03f176aca25c668726e5059a04013
Sunday Taiwo <sundaytaiwo@MBP-14-Sun.lan>
2023-07-08 08:59:40 +0100

    all the files

--- services/config.js
+++ services/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const config = {
   db: {
     host: process.env.HOST,
-    user: process.env.user,
+    user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
     port: process.env.DATABASE_PORT
